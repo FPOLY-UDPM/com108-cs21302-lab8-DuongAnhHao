93455c3fa1485d9ab5407b3648618b1987254a64
Duong Anh Hao <haor06480@gmail.com>
2026-02-26 10:49:09 +0700

    nop bai

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Dương Anh Hào]
+ * MSSV:      [PS44395]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH LƯU THÔNG TIN SINH VIÊN 
@@ -10,4 +10,48 @@
 //  Output: Xuất ra thông tin của tất cả sinh viên trên màn hình
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
 
+struct SinhVien {
+    char maSV[20];
+    char ten[50];
+    char nganh[50];
+    float diemTB;
+};
+
+int main() {
+    int n, i;
+
+    printf("Nhap so luong sinh vien: ");
+    scanf("%d", &n);
+
+    struct SinhVien sv[n];
+
+    for(i = 0; i < n; i++) {
+        printf("\nSinh vien thu %d\n", i + 1);
+
+        printf("Ma SV: ");
+        scanf("%s", sv[i].maSV);
+
+        printf("Ten: ");
+        scanf("%s", sv[i].ten);
+
+        printf("Nganh hoc: ");
+        scanf("%s", sv[i].nganh);
+
+        printf("Diem trung binh: ");
+        scanf("%f", &sv[i].diemTB);
+    }
+
+    printf("\n===== DANH SACH SINH VIEN =====\n");
+
+    for(i = 0; i < n; i++) {
+        printf("\nSinh vien thu %d\n", i + 1);
+        printf("Ma SV: %s\n", sv[i].maSV);
+        printf("Ten: %s\n", sv[i].ten);
+        printf("Nganh hoc: %s\n", sv[i].nganh);
+        printf("Diem TB: %.2f\n", sv[i].diemTB);
+    }
+
+    return 0;
+}
--- bai3.c
+++ bai3.c
@@ -1,10 +1,64 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Dương Anh Hào]
+ * MSSV:      [PS44395]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 3: TÌM KIẾM SINH VIÊN
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+struct SinhVien {
+    char maSV[20];
+    char ten[50];
+    float diemTB;
+};
+
+int main() {
+    int n, i;
+    int timThay = 0;
+    char maCanTim[20];
+
+    printf("Nhap so luong sinh vien: ");
+    scanf("%d", &n);
+
+    struct SinhVien sv[n];
+
+    // Nhap danh sach
+    for(i = 0; i < n; i++) {
+        printf("\nSinh vien thu %d\n", i + 1);
+
+        printf("Ma SV: ");
+        scanf("%s", sv[i].maSV);
+
+        printf("Ten: ");
+        scanf("%s", sv[i].ten);
+
+        printf("Diem TB: ");
+        scanf("%f", &sv[i].diemTB);
+    }
+
+    // Nhap ma can tim
+    printf("\nNhap ma sinh vien can tim: ");
+    scanf("%s", maCanTim);
+
+    // Tim kiem
+    for(i = 0; i < n; i++) {
+        if(strcmp(sv[i].maSV, maCanTim) == 0) {
+            printf("\n--- THONG TIN SINH VIEN ---\n");
+            printf("Ma SV: %s\n", sv[i].maSV);
+            printf("Ten: %s\n", sv[i].ten);
+            printf("Diem TB: %.2f\n", sv[i].diemTB);
+            timThay = 1;
+            break;
+        }
+    }
+
+    if(timThay == 0) {
+        printf("Khong tim thay sinh vien!\n");
+    }
+
+    return 0;
+}
